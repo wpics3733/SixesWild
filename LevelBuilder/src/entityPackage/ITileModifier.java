e1d9ecaac524fafb56e68322cf846a058ecf41de
djkiourtsis <djkiourtsis@wpi.edu>
2015-05-02 22:59:32 -0400

    Created javadocs for model classes

--- LevelBuilder/src/entityPackage/ITileModifier.java
+++ LevelBuilder/src/entityPackage/ITileModifier.java
@@ -1,6 +1,14 @@
 package entityPackage;
 
+/**
+ * @author Dean Kiourtsis
+ * Used to modify tiles
+ */
 public interface ITileModifier {
 
+	/**
+	 * Used to modify given tile
+	 * @param tile
+	 */
 	public void modifyTile(Tile tile);
 }
--- LevelBuilder/src/entityPackage/MarkModifier.java
+++ LevelBuilder/src/entityPackage/MarkModifier.java
@@ -1,18 +1,20 @@
 package entityPackage;
 
+/**
+ * @author Dean Kiourtsis
+ * Used to modify a tile's mark
+ */
 public class MarkModifier implements ITileModifier {
 
 	int mark;
 	
 	// constructor
 	MarkModifier(int mark) {
-		// TODO
 		this.mark = mark;
 	}
 
 	@Override
 	public void modifyTile(Tile tile) {
-		// TODO Auto-generated method stub
 		tile.setMark(mark);
 	}
 	
